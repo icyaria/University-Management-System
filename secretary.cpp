b5c89195f85dee443e1a336dc2f9927a8b6a2bc3
icyaria <marykapaki@gmail.com>
2023-11-21 22:24:10 +0200

    βασικη μορφη της κλασης secretary

---
+++ secretary.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Secretary {
+private:
+    
+public:
+    // constructor 
+    Secretary() {
+        
+    }
+
+    // destructor
+    ~Secretary() {
+        
+    }
+
+    //υπερφόρτωση input και output
+    //
+    //
+    //
+
+    //υπερφόρτωση τελεστή +
+    //
+    //
+    //
+
+};
