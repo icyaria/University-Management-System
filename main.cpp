2bc64df8a4f6d0ee1344c2685d11d2724042b3d4
icyaria <marykapaki@gmail.com>
2023-12-14 17:55:07 +0200

    main menu template (?)

--- main.cpp
+++ main.cpp
@@ -13,45 +13,23 @@ int main() {
 
     Secretary sec;
 
-    cout << "Count of people is: "<<Person::getCount() << endl;
-    cout << "\n" << endl;
-    cout << "Default people out of secretary are: "<< endl;
+    cout << "Welcome to University Management System" << endl;
+    cout << "***************************************" << endl;
+    cout << "1. Edit students" << endl;
+    cout << "2. Edit professors" << endl;
+    cout << "3. Edit courses" << endl;
+    cout << "4. Assign courses to professors" << endl;
+    cout << "5. Enroll student" << endl;
+    cout << "6. Get a list of students that passed a course" << endl;
+    cout << "7. See semester statistics (for professors)" << endl;
+    cout << "8. See my grades (for students)" << endl;
+    cout << "9. See students that can graduate" << endl;
+    cout << "10. Exit" << endl;
 
-    Person A ("John", "Smith", "john@gmail.com", 6980336000);
+    int choice;
+    cin >> choice;
 
-    Person B ("Bob", "Williams", "bobwill@gmail.com");
-    cout << A << endl;
-    cout << B << endl;
-
-    cout << "Enter Bob's phone number:";  //έλεγχος προσθήκης στοιχείου στη κλάση
-    long int phoneB;
-    cin >> phoneB;
-    B.setPhone(phoneB);
-
-    cout << "Add new person:" << endl;
-    Person C; //Creates new person out of secretary
-    cin >> C;
-
-    cout << "\nIn main Secretary: " << endl;
-    sec + A;
-    sec + C;
-
-
-    sec.find_person(sec, A);
-    sec.find_person(sec, B);
-
-    cin >> sec; // Adds new person directly to secretary
-    cout<< sec;
-
-    Secretary second_sec(sec); //Copy constructor
-    cout << "In second secretary:" << endl;
-    cout << second_sec;
-    Secretary third_sec = sec; //Assignment operator
-    cout << "In third secretary:" << endl;
-    third_sec + B;
-    cout << third_sec;
-
-    cout << "Count of people is: "<<Person::getCount()<<endl;
+    
 
     return 0;
 }
