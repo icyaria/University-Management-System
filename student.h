4131d92f8a00cd5188aa7555fee00b4cb97963c8
icyaria <marykapaki@gmail.com>
2023-12-26 20:22:37 +0200

    αριθμος μητρωου

--- student.h
+++ student.h
@@ -12,9 +12,10 @@ class Student: public Person {
 private:
     int sem_s;
     int ects_s;
+    long int AM;
 public:
     Student();
-    Student(string f, string l, string e, long int ph, int sem, int ects);
+    Student(string f, string l, string e, long int ph, int sem, int ects, long int am);
     int getSem() const;
     void setSem(int newSem);
     int getEcts() const;
